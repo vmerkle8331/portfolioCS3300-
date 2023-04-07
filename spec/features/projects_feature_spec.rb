bb0f7078c86ccf7a8bfa9b768f6fc2a120cb6efd
vmerkle8331 <vmerkle@uccs.edu>
2023-04-07 01:02:59 +0000

    bullshit

--- spec/features/projects_feature_spec.rb
+++ spec/features/projects_feature_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 RSpec.feature "Projects", type: :feature do
     context "Update project" do
-      let(:project) { Project.create(title: "Test title", description: "Test content") }
+      let!(:project) { Project.create(title: "Test title", description: "Test content") }
       before(:each) do
         visit edit_project_path(project)
       end
