1b6be63cec4c293b2dd5ea2246db2af6b886bac7
vmerkle8331 <vmerkle@uccs.edu>
2023-04-18 21:57:19 +0000

    ‘dumb’

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,7 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: %i[ show edit update destroy ]
-
+  before_action :authenticate_user!, only: %i[ new edit create update destroy]
+  
   # GET /projects or /projects.json
   def index
     @projects = Project.all
