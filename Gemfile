0a1ba72515490b6d6d5ff5da2e4bdf8263c38c7c
vmerkle8331 <vmerkle@uccs.edu>
2023-04-19 00:59:44 +0000

    bullshit

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ end
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+
 end
 
 gem 'mini_racer'
@@ -45,6 +46,10 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails', '~> 4.1.0'
+
+  gem 'pry-rails'
+  gem 'factory_bot_rails'
+  gem 'faker'
 end
 
 
--- spec/features/projects_feature_spec.rb
+++ spec/features/projects_feature_spec.rb
@@ -1,43 +1,67 @@
 require "rails_helper"
 RSpec.feature "Projects", type: :feature do
-    context "Update project" do
-      let!(:project) { Project.create(title: "Test title", description: "Test content") }
-      before(:each) do
-        visit edit_project_path(project)
+    
+  context "Login" do
+    scenario "should sign up" do
+      visit root_path
+      click_link 'Sign up'
+      within("form") do
+        fill_in "Email", with: "testing@test.com"
+        fill_in "Password", with: "123456"
+        fill_in "Password confirmation", with: "123456"
+        click_button "Sign up"
       end
- 
-      scenario "should be successful" do
-        within("form") do
-          fill_in "Title", with: "NTitle"
-        end
-        click_button "Update Project"
-        expect(page).to have_content("Project was successfully updated")
-      end
- 
- 
-      scenario "should fail" do
-        within("form") do
-          fill_in "Title", with: ""
-        end
-        click_button "Update Project"
-        expect(page).to have_content("Title can't be blank")
-      end
-
-      scenario "should be successful" do
-        within("form") do
-          fill_in "Description", with: "New description content"
-        end
-        click_button "Update Project"
-        expect(page).to have_content("Project was successfully updated")
-      end
- 
- 
-      scenario "should fail" do
-        within("form") do
-          fill_in "Description", with: ""
-        end
-        click_button "Update Project"
-        expect(page).to have_content("Description can't be blank")
-      end
+      expect(page).to have_content("Welcome! You have signed up successfully.")
     end
+
+    scenario "should log in" do
+      user = FactoryBot.create(:user)
+      login_as(user)
+      visit root_path
+      expect(page).to have_content("Logged in")
+    end
+  end
+
+  context "Update project" do
+    let!(:project) { Project.create(title: "Test title", description: "Test content") }
+    before(:each) do
+      user = FactoryBot.create(:user)
+      login_as(user)
+      visit edit_project_path(project)
+    end
+ 
+    scenario "should be successful" do
+      within("form") do
+        fill_in "Title", with: "NTitle"
+      end
+      click_button "Update Project"
+      expect(page).to have_content("Project was successfully updated")
+    end
+ 
+ 
+    scenario "should fail" do
+      within("form") do
+        fill_in "Title", with: ""
+      end
+      click_button "Update Project"
+      expect(page).to have_content("Title can't be blank")
+    end
+
+    scenario "should be successful" do
+      within("form") do
+        fill_in "Description", with: "New description content"
+      end
+      click_button "Update Project"
+      expect(page).to have_content("Project was successfully updated")
+    end
+ 
+ 
+    scenario "should fail" do
+      within("form") do
+        fill_in "Description", with: ""
+      end
+      click_button "Update Project"
+      expect(page).to have_content("Description can't be blank")
+    end
+  end
 end
