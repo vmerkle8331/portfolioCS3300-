da045e905569e2a0c563f9143eede92ec90ff6c0
vmerkle8331 <vmerkle@uccs.edu>
2023-04-19 00:29:06 +0000

    stupid

--- spec/controller/projects_functional_spec.rb
+++ spec/controller/projects_functional_spec.rb
@@ -1,5 +1,15 @@
 require "rails_helper"
 RSpec.describe ProjectsController, :type => :controller do
+    
+    # Add this
+    login_user
+
+    let(:valid_attributes) {
+        { :title => "Test title!", :description => "This is a test description", :status => "draft" }
+    }
+    
+    let(:valid_session) { {} }
+    
     describe "test web requests" do
         context "GET #index" do
             it "returns a success response" do
